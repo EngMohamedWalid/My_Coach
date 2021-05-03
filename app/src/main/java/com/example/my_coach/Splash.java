3e11650ec2c783549632974774452934686c9cf5
EngMohamedWalid <74871430+EngMohamedWalid@users.noreply.github.com>
2021-05-03 05:22:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/my_coach/Splash.java
@@ -0,0 +1,31 @@
+package com.example.my_coach;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //to mack activity full screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+         new Handler().postDelayed(() -> {
+             Intent move =new Intent(Splash.this,Login.class);
+             startActivity(move);
+         },3500);
+
+
+    }
+
+
+
+}
